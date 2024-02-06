6abd270eb1168568c2c047cd3c26b5d87969e8d0
Unit659z <sasha862@yandex.ru>
2024-02-06 16:03:27 +0300

    EndLab1

---
+++ Lab1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css"> 
+    <title>Zavodun A.V.</title>
+</head>
+<body>
+<header class="header">
+    <div class="head_wrap">
+        <img class="header_logo" src="poly.png" alt="poly_logo" width="200">
+        <h1 class="head_text">Заводун А.В.</h1>
+    </div>
+</header>
+    <?php
+    echo ("Hello World!")
+    ?>
+</body>
+</html>
