e7fe49e239c5cd069212c046e7a01ea9a9c7cd5c
Unit659z <sasha862@yandex.ru>
2024-02-19 21:31:16 +0300

    EndProj

---
+++ Feedback.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    print_r(get_headers('https://www.google.com/'));
+    ?>
+    <a href="index.html">На первую страницу</a>
+</body>
+</html>
